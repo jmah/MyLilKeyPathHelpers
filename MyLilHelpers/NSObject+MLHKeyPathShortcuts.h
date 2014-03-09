34fc8513e3a647f345cc23118da7f5ce33d3da2e
Jonathon Mah <me@JonathonMah.com>
2014-03-09 01:12:47 -0800

    Document MLHKeyPathShortcuts

--- MyLilHelpers/NSObject+MLHKeyPathShortcuts.h
+++ MyLilHelpers/NSObject+MLHKeyPathShortcuts.h
@@ -17,6 +17,35 @@
 #endif
 
 
+/** MLHKeyPathShortcuts adds accessors to \p NSObject to make it easy to express key-value observing
+ * dependencies with external objects. Consider the method:
+ *
+ \code
+- (BOOL)someFeatureEnabled
+{
+    return self.someFeatureEnabledLocally &&
+           [[NSUserDefaults standardUserDefaults] boolForKey:@"someFeatureEnabledGlobally"];
+}
+ \endcode
+ *
+ * Using MLHKeyPathShortcuts methods, the following dependency method will trigger the correct
+ * notifications for \p someFeatureEnabled :
+ *
+ \code
++ (NSSet *)keyPathsForValuesAffectingSomeFeatureEnabled
+{
+    return [NSSet setWithObjects:@"someFeatureEnabledLocally", @"$defaults.someFeatureEnabledGlobally", nil];
+}
+ \endcode
+ *
+ * \p $classes is the core accessor, enabling key-value coding access to Objective-C classes. For
+ * example, \p [someObject valueForKeyPath:\@"$classes.NSNotificationCenter.defaultCenter"] will
+ * return the same value as \p [NSNotificationCenter defaultCenter].
+ *
+ * Additionally, two accessors are provided for common keys: \p $defaults for
+ * \p [NSUserDefaults standardUserDefaults] , and \p $app for the shared \p NSApplication or
+ * \p UIApplication instance. (You can specify a dependence on \p $app.delegate.someValue .)
+ */
 @interface NSObject (MLHKeyPathShortcuts)
 
 - (id)$classes;
